b44326635cd249d4a488b7dd054c5aadc50bf180
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-03-21 19:42:08 +0400

    Add a test to test uint.MinValue and uint.MaxValue values

---
+++ module1/task1/SummatorAsync/SummatorTest.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+using System.Threading;
+
+namespace SummatorAsync
+{
+	[TestFixture]
+	internal class SummatorTest
+	{
+		private Summator summator = new Summator();
+
+		[Test]
+		[TestCase(uint.MinValue, uint.MinValue)]
+		[TestCase(uint.MaxValue, 9223372034707292160ul)]
+		public void TestSum(uint n, ulong sum)
+		{
+			Assert.AreEqual(summator.Sum(n, default(CancellationToken)).Sum, sum);
+			Assert.AreEqual(summator.Sum(n, default(CancellationToken)).Sum, n * ((ulong)n + 1) / 2);
+		}
+	}
+}
