b3b241b2ebad19bd19d82fdb3cc67790c8b9767f
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-04-09 11:19:07 +0400

    Move dependencies initialization from Main to separate method

--- module4/DocumentsJoiner.WindowsService/Program.cs
+++ module4/DocumentsJoiner.WindowsService/Program.cs
@@ -2,10 +2,7 @@
 using System.Collections.Generic;
 using System.Configuration;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
 using System.Threading;
-using System.Threading.Tasks;
 using DocumentsJoiner.Configuration;
 using DocumentsJoiner.Handlers;
 using DocumentsJoiner.IO;
@@ -16,39 +13,17 @@ namespace DocumentsJoiner.WindowsService
 {
 	class Program
 	{
+		private static DocumentsJoinerWorker documentsJoinerWorker;
+
 		static void Main(string[] args)
 		{
-			Trace.Listeners.Add(new TextWriterTraceListener("log.txt"));
-			var configuration
-				= (DocumentsJoinerConfigurationSection) ConfigurationManager.GetSection("DocumentsJoiner");
-			var detector = new ZXingBarcodeDetector();
-			Func<DocumentsController, SortedDictionary<int, IDocumentHandler>> handlersChainFactory = ctrl =>
-				{
-					var sorted = new SortedDictionary<int, IDocumentHandler>
-					{
-						{1, new DocumentValidator(configuration.BrokenFilesDirectory)},
-						{2, new BarCodeHandler(detector, ctrl)},
-						{3, new ImageHandler(ctrl)},
-					};
-
-					return sorted;
-				};
-			var fileReader = new WaitForFile(configuration.AttemptsToOpenFile);
-			var exceptionsHandler = new ErrorHandler(configuration.BrokenFilesDirectory);
-			Func<DocumentsController> controllersFactory
-				= () => new DocumentsController(
-					handlersChainFactory,
-					exceptionsHandler,
-					fileReader,
-					configuration.Timeout);
-			Func<CancellationToken, IDocumentsJoiner> joinerFactory
-				= token => new PdfSharpDocumentsJoiner(token, fileReader);
+			InitializeDependencies();
 			var rc = HostFactory.Run(x =>
 			{
 				x.Service<DocumentsJoinerWorker>(s =>
 				{
 					s.ConstructUsing(
-						name=> new DocumentsJoinerWorker(configuration, controllersFactory, joinerFactory));
+						name => documentsJoinerWorker);
 					s.WhenStarted(tc => tc.Start());
 					s.WhenStopped(tc => tc.Stop());
 				});
@@ -60,6 +35,36 @@ namespace DocumentsJoiner.WindowsService
 
 			var exitCode = (int) Convert.ChangeType(rc, rc.GetTypeCode());
 			Environment.ExitCode = exitCode;
+		}
+
+		private static void InitializeDependencies()
+		{
+			Trace.Listeners.Add(new TextWriterTraceListener("log.txt"));
+			var configuration
+				= (DocumentsJoinerConfigurationSection) ConfigurationManager.GetSection("DocumentsJoiner");
+			var detector = new ZXingBarcodeDetector();
+			Func<DocumentsController, SortedDictionary<int, IDocumentHandler>> handlersChainFactory = ctrl =>
+			{
+				var sorted = new SortedDictionary<int, IDocumentHandler>
+				{
+					{1, new DocumentValidator(configuration.BrokenFilesDirectory)},
+					{2, new BarCodeHandler(detector, ctrl)},
+					{3, new ImageHandler(ctrl)},
+				};
+
+				return sorted;
+			};
+			var fileReader = new WaitForFile(configuration.AttemptsToOpenFile);
+			var exceptionsHandler = new ErrorHandler(configuration.BrokenFilesDirectory);
+			Func<DocumentsController> controllersFactory
+				= () => new DocumentsController(
+					handlersChainFactory,
+					exceptionsHandler,
+					fileReader,
+					configuration.Timeout);
+			Func<CancellationToken, IDocumentsJoiner> joinerFactory
+				= token => new PdfSharpDocumentsJoiner(token, fileReader);
+			documentsJoinerWorker = new DocumentsJoinerWorker(configuration, controllersFactory, joinerFactory);
 		}
 	}
 }
