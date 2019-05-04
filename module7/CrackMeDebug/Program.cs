ae76faca4b10fdf13f59f6334c427938840bf5a1
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-05-04 19:06:51 +0400

    Add module 7

---
+++ module7/CrackMeDebug/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace CrackMeDebug
+{
+	class Program
+	{
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.Run(new CrackMe.Form1()); 
+		}
+	}
+}
