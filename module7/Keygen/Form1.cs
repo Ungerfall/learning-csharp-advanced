252ceced4b485affb36f7020065afc4a010e8dfc
Leonid Petrov1 <leonid_petrov1@epam.com>
2019-05-23 12:14:07 +0400

    Add keygen

---
+++ module7/Keygen/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Keygen
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private static byte[] dateBytes;
+
+		private void btnGenerate_Click(object sender, EventArgs e)
+		{
+			var addressBytes = System.Net.NetworkInformation.NetworkInterface.GetAllNetworkInterfaces()
+				.FirstOrDefault()
+				?.GetPhysicalAddress()
+				.GetAddressBytes();
+			dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+			int[] source = addressBytes
+				.Select(new Func<byte, int, int>(eval_a))
+				.Select(eval_a)
+				.ToArray();
+
+			textBox1.Text = string.Join("-", source);
+		}
+
+		private int eval_a(int arg)
+		{
+			if (arg <= 999)
+			{
+				return arg * 10;
+			}
+	
+			return arg;
+		}
+
+		private int eval_a(byte addressByte, int index)
+		{
+			return addressByte ^ dateBytes[index];
+		}
+	}
+}
